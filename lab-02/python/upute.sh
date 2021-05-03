1869ff4068db96811f725a3426d71a5e242d0aaf
Siniša Bolšec <sinisa.bolsec0@gmail.com>
2021-05-03 16:14:53 +0200

    zavrsena shell skripta s demonstracijom programa

--- lab-02/python/upute.sh
+++ lab-02/python/upute.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Ova skripta pokrece demonstraciju alata pokrecuci alate
+# Ova skripta pokrece demonstraciju alata pokrecuci alate.
+
+# Moguce je pokretati alate samostalno bez ove skripte.
+# Svaki od alata ima pripadajucu shell skriptu sa kratkim uputama koristenja.
 
 # Za pokretanje potreban je python paket bcrypt "https://pypi.org/project/bcrypt/"
 # Za instalaciju bcrypt paketa potrebno je izvrsiti sljedece naredbe:
@@ -10,29 +13,82 @@
 
 pip install bcrypt
 
+# obrise racune stvorene prethodnim pokretanjem ove skripte
+if [ -e ".storage.pkl" ]; then
+  rm .storage.pkl
+fi
+
+echo ""
 echo ">>> ./usermgmt.sh add sgros"
+echo "### Molim Vas da unesete razlicite lozinke. ###"
 ./usermgmt.sh add sgros
 
+echo ""
+echo ">>> ./usermgmt.sh add sgros"
+echo "### Molim Vas da unesete lozinku duljine manje od 8 znakova. ###"
+./usermgmt.sh add sgros
+
+echo ""
+echo ">>> ./usermgmt.sh add sgros"
+echo "### Molim Vas da unesete jednake lozinke duljine vece ili jednake 8 znakova. ###"
+./usermgmt.sh add sgros
+
+echo ""
 echo ">>> ./login.sh nepostojeci"
+echo "### Unos lozinki nije bitan jer korisnik ne postoji. ###"
 ./login.sh nepostojeci
 
+echo ""
 echo ">>> ./login.sh sgros"
+echo "### Dva puta je moguce unijeti pogresnu lozinku, treci put molim Vas da unesete ispravnu lozinku. ###"
 ./login.sh sgros
 
+echo ""
 echo ">>> ./usermgmt.sh passwd sgros"
+echo "### Molim Vas da unesete razlicite lozinke. ###"
 ./usermgmt.sh passwd sgros
 
+echo ""
+echo ">>> ./usermgmt.sh passwd sgros"
+echo "### Molim Vas da unesete lozinku duljine manje od 8 znakova. ###"
+./usermgmt.sh passwd sgros
+
+echo ""
+echo ">>> ./usermgmt.sh passwd sgros"
+echo "### Molim Vas da unesete jednake lozinke duljine vece ili jednake 8 znakova. ###"
+./usermgmt.sh passwd sgros
+
+echo ""
 echo ">>> ./login.sh sgros"
+echo "### Molim Vas da prvo unesete staru lozinku, a zatim novu lozinku. ###"
 ./login.sh sgros
 
+echo ""
 echo ">>> ./usermgmt.sh forcepass sgros"
+echo "### Ovdje nije potreban unos. Korisnik ce biti prisiljen promijeniti lozinku kod login-a. ###"
 ./usermgmt.sh forcepass sgros
 
+echo ""
 echo ">>> ./login.sh sgros"
+echo "### Molim Vas da unesete ispravnu lozinku, zatim kod promjene lozinke unesete neispravan par lozinka (razlicite lozinke ili duljina < 8). ###"
 ./login.sh sgros
 
+echo ""
+echo ">>> ./login.sh sgros"
+echo "### Molim Vas da unesete ispravnu lozinku, zatim kod promjene lozinke unesete ispravan par lozinka (jednake lozikne i duljina >= 8). ###"
+./login.sh sgros
+
+echo ""
+echo ">>> ./login.sh sgros"
+echo "### Molim Vas da unesete novu promijenjenu loziku. ###"
+./login.sh sgros
+
+echo ""
 echo ">>> ./usermgmt.sh del sgros"
+echo "### Ovdje nije potreban unos. ###"
 ./usermgmt.sh del sgros
 
+echo ""
 echo ">>> ./login.sh sgros"
+echo "### Unos nije bitan jer korisnik vise ne postoji. ###"
 ./login.sh sgros
